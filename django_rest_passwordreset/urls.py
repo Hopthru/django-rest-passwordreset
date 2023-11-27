7e7a19b4c5779293bdebf7caa9f9f2e734dfadcd
Brock Haywood <brock.haywood@gmail.com>
2023-11-27 13:51:21 -0800

    import update re_path location

--- django_rest_passwordreset/urls.py
+++ django_rest_passwordreset/urls.py
@@ -1,12 +1,20 @@
 """ URL Configuration for core auth
 """
-from django.conf.urls import re_path
-from django_rest_passwordreset.views import reset_password_request_token, reset_password_confirm, reset_password_validate_token
+from django.urls import re_path
+from django_rest_passwordreset.views import (
+    reset_password_request_token,
+    reset_password_confirm,
+    reset_password_validate_token,
+)
 
-app_name = 'password_reset'
+app_name = "password_reset"
 
 urlpatterns = [
-    re_path(r'^validate_token/', reset_password_validate_token, name="reset-password-validate"),
-    re_path(r'^confirm/', reset_password_confirm, name="reset-password-confirm"),
-    re_path(r'^', reset_password_request_token, name="reset-password-request"),
+    re_path(
+        r"^validate_token/",
+        reset_password_validate_token,
+        name="reset-password-validate",
+    ),
+    re_path(r"^confirm/", reset_password_confirm, name="reset-password-confirm"),
+    re_path(r"^", reset_password_request_token, name="reset-password-request"),
 ]
